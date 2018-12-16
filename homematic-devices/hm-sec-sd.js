52bfbd5c0a006a8e4c2821197e5c052de92460d6
hobbyquaker <bastiraff@googlemail.com>
2018-12-16 14:54:43 +0100

    refactor hm-sec-* (#31)

--- homematic-devices/hm-sec-sd.js
+++ homematic-devices/hm-sec-sd.js
@@ -1,1 +1,20 @@
-module.exports = require('./hm-sec-sd-2');
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmSecSd extends Accessory {
+    init(config, node) {
+        this.addService('SmokeSensor', config.name)
+            .get('SmokeDetected', config.deviceAddress + ':1.STATE', (value, c) => {
+                return value ? c.SMOKE_DETECTED : c.SMOKE_NOT_DETECTED;
+            })
+
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+                return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+            })
+
+            .fault([
+                config.deviceAddress + ':0.UNREACH',
+                config.deviceAddress + ':1.ERROR_ALARM_TEST',
+                config.deviceAddress + ':1.ERROR_SMOKE_CHAMBER'
+            ])
+    }
+};
--- homematic-devices/hm-sec-tis.js
+++ homematic-devices/hm-sec-tis.js
@@ -1,101 +1,21 @@
-module.exports = class HmSecTis {
-    constructor(config, homematic) {
-        const {bridgeConfig, ccu} = homematic;
-        const {hap} = bridgeConfig;
+const Accessory = require('./lib/accessory');
 
-        homematic.debug('creating Homematic Device ' + config.description.TYPE + ' ' + config.name);
+module.exports = class HmSecTis extends Accessory {
+    init(config, node) {
 
-        const datapointContact = config.iface + '.' + config.description.ADDRESS + ':1.STATE';
-        let valueContact = (ccu.values && ccu.values[datapointContact] && ccu.values[datapointContact].value) ?
-            hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED :
-            hap.Characteristic.ContactSensorState.CONTACT_DETECTED;
+        this.addService('ContactSensor', config.name)
 
-        const datapointLowbat = config.iface + '.' + config.description.ADDRESS + ':0.LOWBAT';
-        let lowbat = (ccu.values && ccu.values[datapointLowbat] && ccu.values[datapointLowbat].value) ?
-            hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW :
-            hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
+            .get('ContactSensorState', config.deviceAddress + ':1.STATE', (value, c) => {
+                return value ? c.CONTACT_NOT_DETECTED : c.CONTACT_DETECTED;
+            })
 
-        const datapointUnreach = config.iface + '.' + config.description.ADDRESS + ':0.UNREACH';
-        let unreach = ccu.values && ccu.values[datapointUnreach] && ccu.values[datapointUnreach].value;
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+                return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+            })
 
-        function getError() {
-            return unreach ? new Error(hap.HAPServer.Status.SERVICE_COMMUNICATION_FAILURE) : null;
-        }
-
-        const acc = bridgeConfig.accessory({id: config.description.ADDRESS, name: config.name});
-        const subtype = '0';
-
-        if (!acc.isConfigured) {
-            acc.getService(hap.Service.AccessoryInformation)
-                .setCharacteristic(hap.Characteristic.Manufacturer, 'eQ-3')
-                .setCharacteristic(hap.Characteristic.Model, config.description.TYPE)
-                .setCharacteristic(hap.Characteristic.SerialNumber, config.description.ADDRESS)
-                .setCharacteristic(hap.Characteristic.FirmwareRevision, config.description.FIRMWARE);
-
-            acc.on('identify', (paired, callback) => {
-                homematic.log('identify ' + config.name + ' ' + config.description.TYPE + ' ' + config.description.ADDRESS);
-                callback();
+            .get('StatusFault', config.deviceAddress + ':0.UNREACH', (value, c) => {
+                return value;
             });
 
-            acc.addService(hap.Service.ContactSensor, config.name, subtype)
-                .updateCharacteristic(hap.Characteristic.ContactSensorState, valueContact)
-                .updateCharacteristic(hap.Characteristic.StatusLowBattery, lowbat)
-                .updateCharacteristic(hap.Characteristic.StatusFault, unreach);
-
-            acc.isConfigured = true;
-        }
-
-        const getListenerContact = callback => {
-            homematic.debug('get ' + config.name + ' 0 ContactSensorState ' + getError() + ' ' + valueContact);
-            callback(null, valueContact);
-        };
-
-        const getListenerLowbat = callback => {
-            homematic.debug('get ' + config.name + ' 0 StatusLowBattery ' + getError() + ' ' + lowbat);
-            callback(null, lowbat);
-        };
-
-        const getListenerFault = callback => {
-            homematic.debug('get ' + config.name + ' 0 StatusFault ' + getError() + ' ' + unreach);
-            callback(null, unreach);
-        };
-
-        acc.getService(subtype).getCharacteristic(hap.Characteristic.ContactSensorState).on('get', getListenerContact);
-        acc.getService(subtype).getCharacteristic(hap.Characteristic.StatusLowBattery).on('get', getListenerLowbat);
-        acc.getService(subtype).getCharacteristic(hap.Characteristic.StatusFault).on('get', getListenerFault);
-
-        const idSubscription = ccu.subscribe({
-            iface: config.iface,
-            device: config.description.ADDRESS,
-            cache: true,
-            change: true
-        }, msg => {
-            switch (msg.channelIndex + '.' + msg.datapoint) {
-                case '0.UNREACH':
-                    unreach = msg.value;
-                    homematic.debug('update ' + config.name + ' 0 StatusFault ' + unreach);
-                    acc.getService(subtype).updateCharacteristic(hap.Characteristic.StatusFault, unreach);
-                    break;
-                case '0.LOWBAT':
-                    lowbat = msg.value ? hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL;
-                    homematic.debug('update ' + config.name + ' 0 StatusLowBattery ' + lowbat);
-                    acc.getService(subtype).updateCharacteristic(hap.Characteristic.StatusLowBattery, lowbat);
-                    break;
-                case '1.STATE':
-                    valueContact = msg.value ? hap.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : hap.Characteristic.ContactSensorState.CONTACT_DETECTED;
-                    homematic.debug('update ' + config.name + ' 0 ContactSensorState ' + valueContact);
-                    acc.getService(subtype).updateCharacteristic(hap.Characteristic.ContactSensorState, valueContact);
-                    break;
-                default:
-            }
-        });
-
-        homematic.on('close', () => {
-            homematic.debug('removing listeners ' + config.name);
-            ccu.unsubscribe(idSubscription);
-            acc.getService(subtype).getCharacteristic(hap.Characteristic.ContactSensorState).removeListener('get', getListenerContact);
-            acc.getService(subtype).getCharacteristic(hap.Characteristic.StatusLowBattery).removeListener('get', getListenerLowbat);
-            acc.getService(subtype).getCharacteristic(hap.Characteristic.StatusFault).removeListener('get', getListenerFault);
-        });
     }
 };
