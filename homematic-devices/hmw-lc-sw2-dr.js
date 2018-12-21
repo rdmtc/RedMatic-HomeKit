c2464b7ff1e0e9e2e6e0c63e502d56795ce38809
hobbyquaker <bastiraff@googlemail.com>
2018-12-21 17:45:39 +0100

    SingleAccessory config option (#93)

--- homematic-devices/hmw-lc-sw2-dr.js
+++ homematic-devices/hmw-lc-sw2-dr.js
@@ -1,6 +1,18 @@
+/* eslint-disable no-new */
+
 const Accessory = require('./lib/accessory');
 
-module.exports = class HmSw2 extends Accessory {
+class SwChan extends Accessory {
+    init(config) {
+        const dp = config.iface + '.' + config.accChannel + '.STATE';
+
+        this.addService('Switch', config.name)
+            .get('On', dp)
+            .set('On', dp);
+    }
+}
+
+class HmwSw2 extends Accessory {
     init(config, node) {
         const {ccu} = node;
 
@@ -18,6 +30,30 @@ module.exports = class HmSw2 extends Accessory {
             this.addService('Switch', name)
                 .get('On', dp)
                 .set('On', dp);
+        }
+    }
+}
+
+module.exports = class HmSw {
+    constructor(config, node) {
+        const {ccu} = node;
+        const multiAcc = config.options && config.options[config.description.ADDRESS + ':SingleAccessory'] && config.options[config.description.ADDRESS + ':SingleAccessory'].disabled;
+
+        if (multiAcc) {
+            for (let i = 3; i <= 4; i++) {
+                const accChannel = config.description.ADDRESS + ':' + i;
+                if (config.options[accChannel] && config.options[accChannel].disabled) {
+                    continue;
+                }
+                const name = ccu.channelNames[accChannel];
+
+                const chConfig = Object.assign({}, config, {accChannel, name});
+                chConfig.description = Object.assign({}, config.description, {ADDRESS: accChannel});
+
+                new SwChan(chConfig, node);
+            }
+        } else {
+            new HmwSw2(config, node);
         }
     }
 };
