108662a695f27fbb7ed24d549218abe62e5a1403
hobbyquaker <bastiraff@googlemail.com>
2018-12-21 16:00:41 +0100

    fix hmip-ps

--- homematic-devices/hmip-ps.js
+++ homematic-devices/hmip-ps.js
@@ -3,7 +3,7 @@ const Accessory = require('./lib/accessory');
 module.exports = class HmipPs extends Accessory {
     init(config) {
         this.addService('Switch', config.name)
-            .get('On', config.deviceAddress +  + ':3.STATE')
-            .set('On', config.deviceAddress +  + ':3.STATE');
+            .get('On', config.deviceAddress + ':3.STATE')
+            .set('On', config.deviceAddress + ':3.STATE');
     }
 };
