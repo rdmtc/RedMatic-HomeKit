b2631956196341867bffc1a3b96fd5aeba45c3c7
hobbyquaker <bastiraff@googlemail.com>
2018-12-23 18:52:29 +0100

    add hb-uni-sen-wea

---
+++ homematic-devices/hb-uni-sen-wea.js
@@ -0,0 +1,20 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HbUniSenWea extends Accessory {
+    init(config) {
+        this.addService('TemperatureSensor', config.name)
+            .setProps('CurrentTemperature', {minValue: -50, maxValue: 120})
+            .get('CurrentTemperature', config.deviceAddress + ':1.TEMPERATURE')
+
+        if (this.option('HumiditySensor')) {
+            this.addService('HumiditySensor', config.name)
+                .get('CurrentRelativeHumidity', config.deviceAddress + ':1.HUMIDITY')
+
+        }
+
+        if (this.option('LightSensor')) {
+            this.addService('LightSensor', config.name)
+                .get('CurrentAmbientLightLevel', config.deviceAddress + ':1.LUX')
+        }
+    }
+};
