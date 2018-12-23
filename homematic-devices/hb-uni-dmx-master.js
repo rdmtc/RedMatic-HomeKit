bd272fcfa5e07effef8b308d483249b0c1d07430
hobbyquaker <bastiraff@googlemail.com>
2018-12-23 20:27:40 +0100

    add hb-uni-dmx, hb-uni-rgb

---
+++ homematic-devices/hb-uni-dmx-master.js
@@ -0,0 +1,33 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmUniDmx extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        this.addService('Switch', ccu.channelNames[config.description.ADDRESS + ':1'])
+            .get('On', this.deviceAddress + ':1.STATE')
+            .set('On', this.deviceAddress + ':1.STATE');
+
+        for (let i = 2; i <= 3; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            const service = this.addService('Switch', ccu.channelNames[ch]);
+
+            service.get('On', () => {
+                return false;
+            });
+            service.set('On', value => {
+                if (value) {
+                    ccu.setValue(config.iface, ch, 'PRESS_SHORT', true)
+                        .then(() => {
+                            callback();
+                        }).catch(() => {
+                            callback(new Error(hap.HAPServer.Status.SERVICE_COMMUNICATION_FAILURE));
+                        });
+                    setTimeout(() => {
+                        service.update('On', false);
+                    }, 250);
+                }
+            });
+        }
+    }
+};
