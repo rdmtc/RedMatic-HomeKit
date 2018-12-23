f8552165415b1f4c084077ff2c80fd5111213652
hobbyquaker <bastiraff@googlemail.com>
2018-12-23 18:46:01 +0100

    fix option

--- homematic-devices/hb-uni-sen-temp-ir.js
+++ homematic-devices/hb-uni-sen-temp-ir.js
@@ -1,1 +1,27 @@
 module.exports = require('./hm-wds30-ot2-sm');
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmSw extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        const channels = config.description.CHILDREN;
+
+        for (let i = 1; i < channels.length; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            if (!this.option(ch)) {
+                continue;
+            }
+
+            const name = ccu.channelNames[channels[i]];
+            const dp = config.iface + '.' + channels[i] + '.TEMPERATURE';
+
+            this.addService('TEMPERATURE', name)
+                .setProps('CurrentTemperature', {minValue: -450, maxValue: 450})
+                .get('CurrentTemperature', dp)
+                .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+                    return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+                });
+        }
+    }
+};
