ab76c398825351aaa92055dcd5e51426b7a75a2a
hobbyquaker <bastiraff@googlemail.com>
2018-12-23 18:46:24 +0100

    add hb-uni-senact*

---
+++ homematic-devices/hb-uni-senact-4-4-sc-bat.js
@@ -0,0 +1,42 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HbUniSenAct extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        const channels = config.description.CHILDREN;
+
+        for (let i = 1; i <= 4; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            if (!this.option(ch)) {
+                continue;
+            }
+
+            const name = ccu.channelNames[channels[i]];
+            const dp = config.iface + '.' + channels[i] + '.STATE';
+
+            this.addService('Switch', name)
+                .get('On', dp)
+                .set('On', dp);
+        }
+
+        for (let i = 5; i <= 8; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            if (!this.option(ch)) {
+                continue;
+            }
+
+            const name = ccu.channelNames[channels[i]];
+            const dp = config.iface + '.' + channels[i] + '.STATE';
+
+            this.addService('ContactSensor', name)
+                .get('ContactSensorState', dp, (value, c) => {
+                    return value ? c.CONTACT_NOT_DETECTED : c.CONTACT_DETECTED;
+                })
+        }
+        this.addService('BatteryService')
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+                return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+            })
+    }
+};
---
+++ homematic-devices/hb-uni-senact-8-8-rc-bat.js
@@ -0,0 +1,28 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HbUniSenAct extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        const channels = config.description.CHILDREN;
+
+        for (let i = 1; i <= 8; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            if (!this.option(ch)) {
+                continue;
+            }
+
+            const name = ccu.channelNames[channels[i]];
+            const dp = config.iface + '.' + channels[i] + '.STATE';
+
+            this.addService('Switch', name)
+                .get('On', dp)
+                .set('On', dp);
+        }
+
+        this.addService('BatteryService')
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+                return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+            })
+    }
+};
