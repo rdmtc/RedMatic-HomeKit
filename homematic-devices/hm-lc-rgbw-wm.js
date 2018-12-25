4189a157651e216847ad48859e6eab68f2c1452a
hobbyquaker <bastiraff@googlemail.com>
2018-12-25 10:31:26 +0100

    implement Hue/Saturation (#91)

--- homematic-devices/hm-lc-rgbw-wm.js
+++ homematic-devices/hm-lc-rgbw-wm.js
@@ -1,33 +1,63 @@
 const Accessory = require('./lib/accessory');
 
-module.exports = class HmUniDmx extends Accessory {
+module.exports = class HmLcRgbw extends Accessory {
     init(config, node) {
-        const {ccu} = node;
+        const {bridgeConfig, ccu} = node;
+        const {hap} = bridgeConfig;
 
-        this.addService('Switch', ccu.channelNames[config.description.ADDRESS + ':1'])
-            .get('On', this.deviceAddress + ':1.STATE')
-            .set('On', this.deviceAddress + ':1.STATE');
+        const hueFactor = 360 / 199;
 
-        for (let i = 2; i <= 3; i++) {
-            const ch = config.description.ADDRESS + ':' + i;
-            const service = this.addService('Switch', ccu.channelNames[ch]);
+        let valueBrightness = 0;
+        let valueColor = 200;
+        let valueSaturation = 0;
 
-            service.get('On', () => {
-                return false;
-            });
-            service.set('On', value => {
-                if (value) {
-                    ccu.setValue(config.iface, ch, 'PRESS_SHORT', true)
-                        .then(() => {
-                            callback();
-                        }).catch(() => {
+        const service = this.addService('Lightbulb', config.name);
+
+        service
+            .get('On', config.deviceAddress + ':1.LEVEL', value => {
+                valueBrightness = value;
+                return value > 0;
+            })
+            .set('On', config.deviceAddress + ':1.LEVEL', value => {
+                if (!valueBrightness || !value) {
+                    return value ? 1 : 0;
+                }
+                return valueBrightness / 100;
+            })
+
+            .get('Brightness', config.deviceAddress + ':1.LEVEL', value => {
+                valueBrightness = value * 100;
+                return value * 100;
+            })
+            .set('Brightness', config.deviceAddress + ':1.LEVEL', value => {
+                valueBrightness = value;
+                return value / 100;
+            })
+
+            .get('Hue', config.deviceAddress + ':2.COLOR', value => {
+                valueColor = value;
+                valueSaturation = valueColor === 200 ? 0 : 100;
+                service.update('Saturation', valueSaturation);
+                return value * hueFactor;
+            })
+            .set('Hue', config.deviceAddress + ':2.COLOR', value => {
+                valueColor = valueSaturation < 10 ? 200 : value / hueFactor;
+                return valueColor;
+            })
+
+            .get('Saturation', callback => {
+                valueSaturation = valueColor === 200 ? 0 : 100;
+                callback(null, valueSaturation);
+            })
+            .set('Saturation', (value, callback) => {
+                valueColor = value < 10 ? 200 : valueColor;
+                ccu.setValue(config.iface, config.description.ADDRESS + ':2', 'COLOR', valueColor)
+                    .then(() => {
+                        callback();
+                    })
+                    .catch(() => {
                         callback(new Error(hap.HAPServer.Status.SERVICE_COMMUNICATION_FAILURE));
                     });
-                    setTimeout(() => {
-                        service.update('On', false);
-                    }, 250);
-                }
             });
-        }
     }
 };
