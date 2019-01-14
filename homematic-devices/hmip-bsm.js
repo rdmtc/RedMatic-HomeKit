dca713a56da39ca607b800f95cca6de41f946023
hobbyquaker <bastiraff@googlemail.com>
2019-01-14 19:05:56 +0100

    use service Lightbulb for HmIP-BSM

--- homematic-devices/hmip-bsm.js
+++ homematic-devices/hmip-bsm.js
@@ -2,7 +2,7 @@ const Accessory = require('./lib/accessory');
 
 module.exports = class HmipBsm extends Accessory {
     init(config) {
-        this.addService('Switch', config.name)
+        this.addService('Lightbulb', config.name)
             .get('On', config.deviceAddress + ':4.STATE')
             .set('On', config.deviceAddress + ':4.STATE');
     }
