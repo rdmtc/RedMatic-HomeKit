f1d27441c86470097f3dbe35e7af7d90967766ce
hobbyquaker <bastiraff@googlemail.com>
2019-03-10 15:01:56 +0100

    add HmIPW-DRS*

---
+++ homematic-devices/hmipw-drs4.js
@@ -0,0 +1,97 @@
+/* eslint-disable no-new */
+
+const Accessory = require('./lib/accessory');
+
+function addService(type, dp, name) {
+    switch (type) {
+        case 'ValveIrrigation':
+        // intentional fallthrough
+        case 'Valve': {
+            const service = this.addService('Valve', name, type);
+
+            service.update('ValveType', type === 'ValveIrrigation' ? 1 : 0);
+
+            service
+                .get('Active', dp, val => val ? 1 : 0)
+                .get('InUse', dp, val => val ? 1 : 0)
+                .set('Active', dp, val => {
+                    service.update('InUse', val);
+                    return Boolean(val);
+                });
+            break;
+        }
+        case 'Lightbulb':
+        // intentional fallthrough
+        case 'Fan':
+        // intentional fallthrough
+        case 'Outlet':
+        // intentional fallthrough
+        default:
+            this.addService(type, name, type === 'Switch' ? '' : type)
+                .get('On', dp)
+                .set('On', dp);
+    }
+}
+
+class AccSingleService extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+        const dp = config.iface + '.' + config.accChannel + '.STATE';
+        const name = ccu.channelNames[config.accChannel];
+        const type = this.option('', 'type') || 'Switch';
+
+        node.debug(config.accChannel + ' ' + type + ' ' + this.option('', 'type'));
+
+        addService.call(this, type, dp, name);
+    }
+}
+
+class AccMultiService extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+        const channels = config.description.CHILDREN;
+
+        for (let i = 2; i < (channels.length - 1); i += 4) {
+            const ch = channels[i];
+            if (!this.option(i)) {
+                continue;
+            }
+            const name = ccu.channelNames[ch];
+            const dp = config.iface + '.' + ch + '.STATE';
+            const type = this.option(i, 'type') || 'Switch';
+
+            node.debug(i + ' ' + type + ' ' + this.option(i, 'type'));
+
+            addService.call(this, type, dp, name);
+        }
+    }
+}
+
+module.exports = class HmipwDrs {
+    option(id) {
+        return !(this.config.options[id] && this.config.options[id].disabled);
+    }
+
+    constructor(config, node) {
+        const {ccu} = node;
+        this.ccu = ccu;
+        this.config = config;
+        if (this.option(config.description.ADDRESS + ':SingleAccessory')) {
+            new AccMultiService(config, node);
+        } else {
+            const channels = config.description.CHILDREN;
+            for (let i = 2; i < (channels.length - 1); i += 4) {
+                const ch = channels[i];
+                if (!this.option(ch)) {
+                    continue;
+                }
+                const name = ccu.channelNames[ch];
+
+                const chConfig = Object.assign({}, config, {accChannel: ch, name});
+                chConfig.description = Object.assign({}, config.description, {ADDRESS: ch});
+
+                new AccSingleService(chConfig, node);
+            }
+        }
+    }
+};
