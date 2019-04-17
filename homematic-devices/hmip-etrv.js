0733a1007a6ddc621d11ebc3d9429006271773e4
hobbyquaker <bastiraff@googlemail.com>
2019-04-17 19:28:20 +0200

    fix low battery datapoint

--- homematic-devices/hmip-etrv.js
+++ homematic-devices/hmip-etrv.js
@@ -103,7 +103,7 @@ module.exports = class HmipEtrv extends Accessory {
         });
 
         this.addService('BatteryService', config.name)
-            .get('StatusLowBattery', config.deviceAddress + ':0.LOWBAT', (value, c) => {
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOW_BAT', (value, c) => {
                 return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
             })
             .get('BatteryLevel', config.deviceAddress + ':0.OPERATING_VOLTAGE', this.percent)
