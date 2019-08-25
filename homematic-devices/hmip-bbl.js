8eb3d9c884dc49e0a222cf3991c3eb875df837d5
hobbyquaker <bastiraff@googlemail.com>
2019-08-25 21:35:45 +0200

    send both LEVEL and LEVEL_2 (#168)

--- homematic-devices/hmip-bbl.js
+++ homematic-devices/hmip-bbl.js
@@ -2,10 +2,12 @@ const Accessory = require('./lib/accessory');
 
 module.exports = class HmipBbl extends Accessory {
     init(config, node) {
-        const {ccu} = node;
+        const {ccu, bridgeConfig} = node;
+        const {hap} = bridgeConfig;
 
-        let intermediatePosition;
-        let targetPosition;
+        let intermediatePosition; // 0-100
+        let LEVEL; // 0.0-1.0
+        let LEVEL_2; // 0.0-1.0
 
         ccu.subscribe({
             datapointName: config.deviceAddress + ':4.LEVEL',
@@ -17,23 +19,23 @@ module.exports = class HmipBbl extends Accessory {
 
         const service = this.addService('WindowCovering', config.name);
 
-        service.get('CurrentPosition', config.deviceAddress + ':4.LEVEL', value => {
-            targetPosition = value;
-            intermediatePosition = value * 100;
-            return value * 100;
-        })
-
-            .get('TargetPosition', config.deviceAddress + ':4.LEVEL', value => {
-                if (typeof targetPosition === 'undefined') {
-                    targetPosition = value;
-                }
-
-                return targetPosition * 100;
+        service
+            .get('CurrentPosition', config.deviceAddress + ':4.LEVEL', value => {
+                LEVEL = value;
+                intermediatePosition = value * 100;
+                return LEVEL * 100;
             })
 
-            .set('TargetPosition', config.deviceAddress + ':4.LEVEL', value => {
-                targetPosition = value / 100;
-                targetPosition = value / 100;
+            .get('TargetPosition', config.deviceAddress + ':4.LEVEL', value => {
+                if (typeof LEVEL === 'undefined') {
+                    LEVEL = value;
+                }
+
+                return LEVEL * 100;
+            })
+
+            .set('TargetPosition', (value, callback) => {
+                LEVEL = value / 100;
                 if (value === 0 && intermediatePosition === 0) {
                     intermediatePosition = 1;
                 } else if (value === 100 && intermediatePosition === 100) {
@@ -42,7 +44,19 @@ module.exports = class HmipBbl extends Accessory {
 
                 node.debug(config.name + ' intermediatePosition ' + intermediatePosition);
                 service.update('CurrentPosition', intermediatePosition);
-                return targetPosition;
+
+                const params = {
+                    LEVEL,
+                    LEVEL_2
+                };
+                node.debug('set ' + config.name + ' (WindowCovering) TargetPosition ' + value + ' -> ' + config.description.ADDRESS + ':4 ' + JSON.stringify(params));
+                ccu.methodCall(config.iface, 'putParamset', [config.description.ADDRESS + ':4', 'VALUES', params])
+                    .then(() => {
+                        callback();
+                    })
+                    .catch(() => {
+                        callback(new Error(hap.HAPServer.Status.SERVICE_COMMUNICATION_FAILURE));
+                    });
             })
 
             .get('PositionState', config.deviceAddress + ':4.ACTIVITY_STATE', (value, c) => {
@@ -57,15 +71,29 @@ module.exports = class HmipBbl extends Accessory {
             })
 
             .get('CurrentVerticalTiltAngle', config.deviceAddress + ':4.LEVEL_2', value => {
-                return (value * 180) - 90;
+                LEVEL_2 = (value * 180) - 90;
+                return LEVEL_2;
             })
 
             .get('TargetVerticalTiltAngle', config.deviceAddress + ':4.LEVEL_2', value => {
-                return (value * 180) - 90;
+                LEVEL_2 = (value * 180) - 90;
+                return LEVEL_2;
             })
 
-            .set('TargetVerticalTiltAngle', config.deviceAddress + ':4.LEVEL_2', value => {
-                return (value + 90) / 180;
+            .set('TargetVerticalTiltAngle', (value, callback) => {
+                LEVEL_2 = (value + 90) / 180;
+                const params = {
+                    LEVEL,
+                    LEVEL_2
+                };
+                node.debug('set ' + config.name + ' (WindowCovering) TargetVerticalTiltAngle ' + value + ' -> ' + config.description.ADDRESS + ':4 ' + JSON.stringify(params));
+                ccu.methodCall(config.iface, 'putParamset', [config.description.ADDRESS + ':4', 'VALUES', params])
+                    .then(() => {
+                        callback();
+                    })
+                    .catch(() => {
+                        callback(new Error(hap.HAPServer.Status.SERVICE_COMMUNICATION_FAILURE));
+                    });
             });
     }
 };
