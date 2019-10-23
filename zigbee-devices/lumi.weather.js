d8bd1a5ae0445aaee6ecdd03dc2a55c67ce31578
hobbyquaker <bastiraff@googlemail.com>
2019-10-23 20:31:35 +0200

    add zigbee devices lumi.sens and lumi.sensor_ht

--- zigbee-devices/lumi.weather.js
+++ zigbee-devices/lumi.weather.js
@@ -6,7 +6,7 @@ module.exports = class LumiWeather extends Accessory {
     }
 
     static get modelID() {
-        return ['lumi.weather'];
+        return ['lumi.weather', 'lumi.sens', 'lumi.sensor_ht'];
     }
 
     init(device) {
