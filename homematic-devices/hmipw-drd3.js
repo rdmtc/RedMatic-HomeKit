9f41575bb92890403e2cd4db000429db2afc6ff9
hobbyquaker <bastiraff@googlemail.com>
2020-01-02 15:17:05 +0100

    add HmIPW-DRD3 (#241)

---
+++ homematic-devices/hmipw-drd3.js
@@ -0,0 +1,54 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmipwDrd extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        let valueBrightness = 0;
+
+        for (let j = 0; j < 3; j++) {
+            for (let c = 0; c < 3; c++) {
+                const i = (j * 4) + (c + 2);
+                if ((c === 0 && this.option(i)) || (c !== 0 && this.option(i, 'enabled'))) {
+                    const channel = config.deviceAddress + ':' + i;
+                    const name = ccu.channelNames[channel];
+
+                    this.addService('Lightbulb', name)
+
+                        .get('On', channel + '.LEVEL', value => {
+                            valueBrightness = value;
+                            return value > 0;
+                        })
+
+                        .set('On', (value, callback) => {
+                            if (value) {
+                                setTimeout(() => {
+                                    if (valueBrightness === 0) {
+                                        value = 1;
+                                    } else {
+                                        value = valueBrightness / 100;
+                                    }
+
+                                    this.ccuSetValue(channel + '.LEVEL', value, callback);
+                                }, 100);
+                            } else {
+                                this.ccuSetValue(channel + '.LEVEL', 0, callback);
+                            }
+                        })
+
+                        .get('Brightness', channel + '.LEVEL', value => {
+                            valueBrightness = value * 100;
+                            return value * 100;
+                        })
+
+                        .set('Brightness', channel + '.LEVEL', value => {
+                            valueBrightness = value;
+                            return value / 100;
+                        });
+                }
+            }
+        }
+
+
+    }
+};
