1e09f6adee92929f72d797eb2b3c634740cdd0c5
kneip68 <44921902+kneip68@users.noreply.github.com>
2020-03-30 16:59:58 +0200

    Initial ColorXY plus attach to IKEA CWS

--- zigbee-devices/zll.colorXY.js
+++ zigbee-devices/zll.colorXY.js
@@ -1,21 +1,40 @@
+// Just as a reminder some references for Clusters both in HEX as well as in DEC
+// HEX    DEC       Cluster
+// 0x0000 0         Basic
+// 0x0003 3         Identify
+// 0x0004 4         Groups
+// 0x0005 5         Scenes
+// 0x0006 6         OnOff
+// 0x0008 8         Level Control
+// 0x0019 25        OTA
+// 0x0020 32        
+// 0x0300 768       Colour Control
+// 0x0B05 2821      Diagnostics
+// 0x1000 4096      Touchlink ZLL Commissioning
+// 0xFC0F 64527     OSRAM: Safe State for ON?
+
+
 const Accessory = require('./lib/accessory');
 
 module.exports = class ZllColor extends Accessory {
     static get manufacturerName() {
-        return [];
+        return ['IKEA of Sweden'];
     }
 
     static get modelID() {
         return [
+            'TRADFRI bulb E27 CWS opal 600lm',
+            'TRADFRI bulb E26 CWS opal 600lm',
+            'TRADFRI bulb E14 CWS opal 600lm'
         ];
     }
 
     static get deviceID() {
-        return [0x200];
+        return [];
     }
 
     init(device) {
-        this.node.debug(`init zll.color ${this.device.ieeeAddr} ${this.device.meta.name}`);
+        this.node.debug(`init zll.colorXY ${this.device.ieeeAddr} ${this.device.meta.name}`);
         const ep = device.endpoints[0].ID;
         this.addService('Lightbulb', device.meta.name)
             .get('On', ep, 'genOnOff', 'onOff', data => Boolean(data))
