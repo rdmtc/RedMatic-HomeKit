f16a1e737d76f51ee0a4ce4af248ae80ef816d35
kneip68 <44921902+kneip68@users.noreply.github.com>
2020-04-02 14:35:57 +0200

    Update zll.colorXY.js
    Copied new code for color capability handling to XY class for Tradfri

--- zigbee-devices/zll.colorXY.js
+++ zigbee-devices/zll.colorXY.js
@@ -16,7 +16,7 @@
 
 const Accessory = require('./lib/accessory');
 
-module.exports = class ZllColor extends Accessory {
+module.exports = class ZllColorXY extends Accessory {
     static get manufacturerName() {
         return ['IKEA of Sweden'];
     }
@@ -34,9 +34,10 @@ module.exports = class ZllColor extends Accessory {
     }
 
     init(device) {
-        this.node.debug(`init zll.colorXY ${this.device.ieeeAddr} ${this.device.meta.name}`);
+        this.node.debug(`init zll.extendendcolor ${this.device.ieeeAddr} ${this.device.meta.name}`);
         const ep = device.endpoints[0].ID;
-        this.addService('Lightbulb', device.meta.name)
+        let colormode;
+        const service = this.addService('Lightbulb', device.meta.name)
             .get('On', ep, 'genOnOff', 'onOff', data => Boolean(data))
             .set('On', ep, 'genOnOff', data => {
                 return {command: data ? 'on' : 'off', payload: {}};
@@ -47,14 +48,49 @@ module.exports = class ZllColor extends Accessory {
                 return {command: 'moveToLevel', payload: {level: Math.round(data * 2.54), transtime: 0}};
             })
 
-            .get('Hue', ep, 'lightingColorCtrl', 'enhancedCurrentHue', data => Math.round(data / 65535 * 360))
-            .set('Hue', ep, 'lightingColorCtrl', data => {
-                return {command: 'enhancedMoveToHue', payload: {enhancehue: Math.round(data * 65535 / 360), direction: 0, transtime: 0}};
-            })
+            .get('ColorTemperature', ep, 'lightingColorCtrl', 'colorTemperature', data => colormode === 'ct' ? data : null)
+            .set('ColorTemperature', ep, 'lightingColorCtrl', data => {
+                const current = this.device.getEndpoint(ep) && this.device.getEndpoint(ep).clusters['lightingColorCtrl'] && this.device.getEndpoint(ep).clusters['lightingColorCtrl'].attributes && this.device.getEndpoint(ep).clusters['lightingColorCtrl'].attributes['colorTemperature']
+                if (data !== current) {
+                    colormode = 'ct';
+                    return {command: 'moveToColorTemp', payload: {colortemp: data, transtime: 0}};
+                }
+            }, false)
+            .setProps('ColorTemperature', {minValue: 153, maxValue: 370})
 
-            .get('Saturation', ep, 'lightingColorCtrl', 'currentSaturation', data => Math.round(data / 2.54))
+            .get('Hue', ep, 'lightingColorCtrl', 'enhancedCurrentHue', data => colormode === 'ct' ? null : Math.round(data / 65535 * 360))
+            .set('Hue', ep, 'lightingColorCtrl', data => {
+                colormode = 'hs';
+                return {
+                    command: 'enhancedMoveToHue',
+                    payload: {enhancehue: Math.round(data * 65535 / 360), direction: 0, transtime: 0}
+                };
+            }, false)
+
+            .get('Saturation', ep, 'lightingColorCtrl', 'currentSaturation', data => colormode === 'ct' ? null : Math.round(data / 2.54))
             .set('Saturation', ep, 'lightingColorCtrl', data => {
-                return {command: 'moveToSaturation', payload: {saturation: Math.round(data * 2.54), transtime: 0}};
+                const saturation = Math.round(data * 2.54);
+                if (saturation !== this.device.getEndpoint(ep).clusters['lightingColorCtrl'].attributes['currentSaturation']) {
+                    //colormode = 'hs';
+                    return {command: 'moveToSaturation', payload: {saturation: saturation, transtime: 0}};
+                }
+
+
+            }, false)
+
+            .sub(ep, 'lightingColorCtrl', 'colorMode', colorMode => {
+                console.log('colorMode', colorMode);
+                switch (colorMode) {
+                    case 0:
+                        colormode = 'hs';
+                        break;
+                    case 1:
+                        colormode = 'xy';
+                        break;
+                    case 2:
+                        colormode = 'ct';
+                        break;
+                }
             });
     }
 };
