1e095baf3f27f2f2aeb59d463427a3f996cb149c
hobbyquaker <bastiraff@googlemail.com>
2021-03-11 21:05:54 +0100

    add HmIP-STE2-PCB (#324)

---
+++ homematic-devices/hmip-ste2-pcb.js
@@ -0,0 +1,26 @@
+const Accessory = require('./lib/accessory');
+
+module.exports = class HmipSte2Pcb extends Accessory {
+    init(config, node) {
+        const {ccu} = node;
+
+        const channels = config.description.CHILDREN;
+
+        for (let i = 1; i < channels.length; i++) {
+            const ch = config.description.ADDRESS + ':' + i;
+            if (config.options[ch] && config.options[ch].disabled) {
+                continue;
+            }
+
+            const name = ccu.channelNames[channels[i]];
+            const dp = config.iface + '.' + channels[i] + '.ACTUAL_TEMPERATURE';
+
+            this.addService('TemperatureSensor', name)
+            .setProps('CurrentTemperature', {minValue: -150, maxValue: 150})
+            .get('CurrentTemperature', dp)
+            .get('StatusLowBattery', config.deviceAddress + ':0.LOW_BAT', (value, c) => {
+                return value ? c.BATTERY_LEVEL_LOW : c.BATTERY_LEVEL_NORMAL;
+            });
+        }
+    }
+};
